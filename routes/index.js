a24212085d75d7baa637454da6a8741dd241e459
birme <jonas.birme@eyevinn.se>
2016-06-07 16:27:24 +0200

    Fixed path to stylesheets

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'HTML5 HLS Player by Eyevinn Technology' });
 });
 
 module.exports = router;
