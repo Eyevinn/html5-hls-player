c0969b19f2ec051be6932882fc8d8bf2250fc38f
birme <jonas.birme@eyevinn.se>
2016-06-08 17:00:24 +0200

    Ability to change quality level

--- public/javascripts/html5-player.js
+++ public/javascripts/html5-player.js
@@ -22,27 +22,17 @@ function playVideo() {
 function play(videosrc) {
   player = document.getElementById('video-container');
   var hls = new Hls();
-  $('#data_version').html("hls.js 0.5.34"); 
+  $('#data_version').html("hls.js " + Hls.version); 
   hls.loadSource(videosrc);
   hls.attachMedia(player);
-  hls.on(Hls.Events.MANIFEST_PARSED, function() {
+  hls.on(Hls.Events.MANIFEST_PARSED, function(ev, data) {
+    initQualitySelector(hls, data.levels);
     player.play();
   });
 
   player.addEventListener('timeupdate', function(ev) {
-    var p = this;
-/*
-    var tech = p.tech({ IWillNotUseThisInPlugins: true });
-    var hls = tech.hls;
-    if(hls) {
-      $('#data_bandwidth').html(Math.round(hls.stats.bandwidth / 8 / 1000) + " kbps ("+ Math.round(hls.stats.mediaBytesTransferred / 1024 / 1024) + " MB transferred)");
-    } else {
-      $('#data_bandwidth').html("n/a");
-    }
-    $('#data_currenttime').html(Math.round(tech.currentTime()) + " sec of " + tech.duration() + " sec");
-    var buf = tech.buffered;
-    $('#data_buffered').html(buf.length + " ranges buffered");
-*/
+    var currentlev = hls.levels[hls.currentLevel];
+    $('#data_bandwidth').html(currentlev.width + "x" + currentlev.height + " (" + currentlev.bitrate/1000 + ") kbps"); 
   });
   player.addEventListener('error', qos_events);
   player.addEventListener('progress', qos_events);
@@ -50,6 +40,29 @@ function play(videosrc) {
   player.addEventListener('stalled', qos_events);
   player.addEventListener('playing', qos_events);
   player.addEventListener('ratechange', qos_events);
+}
+
+function initQualitySelector(hls, levels) {
+  var sel = $("#level");
+  sel.empty();
+  var defopt = $('<option></option>').attr("value", "auto").text("Auto");
+  defopt.attr('selected', true);
+  sel.append(defopt);
+  var quality_html = '';
+  for (l=0; l<levels.length; l++) {
+    var lev = levels[l];
+    var option = $('<option></option>').attr("value", l).text(lev.width + "x" + lev.height + " ("+lev.bitrate/1000+" kbps)");
+    sel.append(option);
+    quality_html = quality_html + lev.width + "x" + lev.height + " ("+ Math.round(lev.bitrate/1000)  +" kbps)<br>";
+  }
+  $('#data_qualitylevels').html(quality_html);
+  sel.on("change", function(ev) {
+    if(ev.target.value != "auto") {
+      hls.loadLevel = ev.target.value;
+    } else {
+      hls.loadLevel = -1;
+    } 
+  });
 }
 
 function qos_events(ev) {
